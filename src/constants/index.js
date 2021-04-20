2e4a3dfcd78e6c0d7d6eb85ae4d8c0a5cac0bce8
Taiwo Adepoju <adepojutaiwoandrew@gmail.com>
2021-04-20 03:36:08 +0100

    chore(constants): Adds constants

---
+++ src/constants/index.js
@@ -0,0 +1,18 @@
+// API URL
+const TODO_API = 'https://jsonplaceholder.typicode.com/todos';
+
+// Redux action types
+const FETCH_TODOS = 'FETCH_TODOS';
+const FETCH_TODOS_FAILURE = 'FETCH_TODOS_FAILURE';
+const FETCH_TODOS_SUCCESS = 'FETCH_TODOS_SUCCESS';
+const ADD_TODO = 'ADD_TODO';
+
+const constants = {
+  TODO_API,
+  FETCH_TODOS,
+  FETCH_TODOS_FAILURE,
+  FETCH_TODOS_SUCCESS,
+  ADD_TODO,
+};
+
+export default constants;
